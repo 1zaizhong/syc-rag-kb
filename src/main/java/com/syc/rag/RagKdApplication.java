1fb48227aca85b411fb9b248a67eb69f12416b73
zai_zhong_ <zai_zhong_@noreply.gitcode.com>
2026-04-28 20:41:22 +0800

    本地配置redis，postgres，大模型API等配置

---
+++ src/main/java/com/syc/rag/RagKdApplication.java
@@ -0,0 +1,17 @@
+package com.syc.rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 粟英朝
+ * @version 0.0.3
+ * @date 2026/4/28 下午6:26
+ * @description
+ **/
+@SpringBootApplication
+public class RagKdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RagKdApplication.class, args);
+    }
+}
